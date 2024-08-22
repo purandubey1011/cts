9e44d0253c09251b27c2925fc1ca13a6b8ac597f
purandubey1011 <dubeypuran2002@gmail.com>
2024-08-22 04:59:22 -0700

    uri changes

--- src/models/database.js
+++ src/models/database.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 exports.connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL        );
+        await mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000 });
         console.log("Database connected successfully");
     } catch (error) {
         console.log(error.message);
