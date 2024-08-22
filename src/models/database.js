1877a2f78ff950aa04d21fc25533ac3e4db4e8b6
purandubey1011 <dubeypuran2002@gmail.com>
2024-08-22 05:28:51 -0700

    back to change

--- src/models/database.js
+++ src/models/database.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 exports.connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL,{ serverSelectionTimeoutMS: 30000 });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Database connected successfully");
     } catch (error) {
         console.log(error.message);
