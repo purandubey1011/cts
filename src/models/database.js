460203f2ba128713a1b5000347ca681970531841
purandubey1011 <dubeypuran2002@gmail.com>
2024-08-22 04:22:53 -0700

    database name added in mongo uri

--- src/models/database.js
+++ src/models/database.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 exports.connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
         console.log("Database connected successfully");
     } catch (error) {
         console.log(error.message);
